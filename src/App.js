1d34af378325be527109a3c5ffc5cc7fcb0d4f30
victorfdt <victorfdt@yahoo.com.br>
2018-12-20 13:11:15 +0000

    Creating folders and added react-bootstrap

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Button } from 'react-bootstrap'
 import logo from './logo.svg';
 import './App.css';
 
@@ -8,6 +9,7 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
+          <Button bsStyle="primary">Primary</Button>
           <p>
             Edit <code>src/App.js</code> and save to reload.
           </p>
